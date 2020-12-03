5c37486a0df4e9c459350b5c555bc307be961a8c
remindchobits <remindchobits@users.noreply.github.com>
2020-12-03 15:01:54 +0100

    Update counterfactual.py

--- counterfactual.py
+++ counterfactual.py
@@ -4,16 +4,16 @@ import dice_ml
 from model import Net
 from dataset.adult import Adult
 
-#generate_CF takes a dictionary as an input which has the column name as its keys and the value as its corresponding values
-#generate_CF returns a dataframe
+#generate_CF takes a Dataframe as an input and returns a Dataframe with a series of CFs
 def generate_CF(instance):
     ds_train = Adult('dataset', train=True).get_original_features()
     d = dice_ml.Data(dataframe=ds_train, continuous_features=['age', 'fnlwgt', 'educational-num', 'capital-gain','capital-loss', 'hours-per-week'], outcome_name='income')
     backend = 'PYT'
     ML_modelpath = "model_ori.pth"
-    m = dice_ml.Model(model_path= ML_modelpath, backend=backend)
+    m = dice_ml.Model(model_path = ML_modelpath, backend = backend)
     exp = dice_ml.Dice(d, m)
-    dice_exp = exp.generate_counterfactuals(instance, total_CFs=5, desired_class="opposite",
+    instance = pd.DataFrame.to_dict(instance,orient ='record')
+    dice_exp = exp.generate_counterfactuals(instance[0], total_CFs=5, desired_class="opposite",
                                             proximity_weight=0.5, diversity_weight=1, categorical_penalty=0.1, 
                                             algorithm="DiverseCF", features_to_vary="all", yloss_type="hinge_loss", 
                                             diversity_loss_type="dpp_style:inverse_dist", 
@@ -23,15 +23,12 @@ def generate_CF(instance):
                                             init_near_query_instance=True, tie_random=False, 
                                             stopping_threshold=0.5, posthoc_sparsity_param=0.1, 
                                             posthoc_sparsity_algorithm="binary")
-    res = dice_exp.visualize_as_dataframe(show_only_changes=True)
+    res = dice_exp.visualize_as_dataframe(show_only_changes=False)
     return res
 
 
-
-def main():
-    #exapmle
-    res = generate_CF({'age': 0.682426, 'workclass': 'Private', 'fnlwgt': 0.852807, 'education': 'HS-grad', 'educational-num': -0.419331,'marital-status':'Married-civ-spouse', 'occupation': 'Machine-op-inspct', 'relationship': 'Husband', 'race':'White', 'gender':'Male','capital-gain':0.271595, 'capital-loss': -0.217125, 'hours-per-week':0.61152, 'native-country':'United-States'})
-    print(res)
-
-if __name__ == '__main__':
-    main()
+#Example
+ori_dict = {'age': 0.682426, 'workclass': 'Private', 'fnlwgt': 0.852807, 'education': 'HS-grad', 'educational-num': -0.419331,'marital-status':'Married-civ-spouse', 'occupation': 'Machine-op-inspct', 'relationship': 'Husband', 'race':'White', 'gender':'Male','capital-gain':0.271595, 'capital-loss': -0.217125, 'hours-per-week':0.61152, 'native-country':'United-States'}
+instance = pd.DataFrame(ori_dict,index=[0])
+res = generate_CF(instance)
+print(res)
